7f377e2e73bec9fc9dcaada2a0f1d67df964eebc
ShahTheShah <NeatDeveloper@ya.ru>
2024-10-09 04:22:14 +0300

    Init "Hello, world!" logic

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { Hono } from 'hono';
+
+const server = new Hono();
+
+server.get('/', async __context__ => {
+    return __context__.text('Hello, world!');
+});
+
+
+export default {
+    port: 3000,
+    fetch: server.fetch
+}
