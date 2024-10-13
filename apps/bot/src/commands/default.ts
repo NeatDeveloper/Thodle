92aad8f8dddcd710c3b13de4b3a4c26acab8e017
ShahTheShah <NeatDeveloper@ya.ru>
2024-10-13 21:07:09 +0300

    @app/bot: fixing and add midllewares

--- apps/bot/src/commands/default.ts
+++ apps/bot/src/commands/default.ts
@@ -3,7 +3,7 @@ import { Composer } from 'grammy';
 const commands = new Composer<APP.Context>();
 
 commands.command('start', async __context__ => {
-    console.log(__context__.user)
+    console.log(__context__)
     await __context__.reply('Hello, world!');
 });
 
--- apps/bot/src/context/declaration.d.ts
+++ apps/bot/src/context/declaration.d.ts
@@ -4,17 +4,5 @@ import BotContext from '.';
 export namespace ContextDeclaration {
     type Base = BotContext;
 
-    interface UserData {
-        id: number;
-        lastName?: string;
-        firstName?: string;
-        username?: string;
-        isBot: boolean;
-        lang: LanguageCode;
-        isPremium: boolean;
-    }
-
-    interface Implement {
-        user?: UserData;
-    }
+    interface Implement { }
 }
