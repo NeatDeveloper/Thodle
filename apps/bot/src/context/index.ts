91b57226d30811155abc6d8a4acda79f261b67ef
ShahTheShah <NeatDeveloper@ya.ru>
2024-10-13 21:07:09 +0300

    @app/bot: fixing and add midllewares

--- apps/bot/src/context/index.ts
+++ apps/bot/src/context/index.ts
@@ -1,5 +1,5 @@
 import { Api, Context as GrammyContext, type RawApi } from 'grammy';
-import { LanguageCodes, type LanguageCode, type Update, type UserFromGetMe } from 'grammy/types';
+import { type Update, type UserFromGetMe } from 'grammy/types';
 
 import type { ContextDeclaration } from './declaration';
 
@@ -7,17 +7,5 @@ import type { ContextDeclaration } from './declaration';
 export default class Context extends GrammyContext implements ContextDeclaration.Implement {
     constructor(update: Update, api: Api<RawApi>, me: UserFromGetMe) {
         super(update, api, me);
-    }
-
-    get user(): ContextDeclaration.UserData {
-        return {
-            id: this.message?.from.id || (this.update?.callback_query?.from.id as number),
-            username: this.message?.from.username || this.update.callback_query?.from.username,
-            lastName: this.message?.from.last_name || this.update.callback_query?.from.last_name,
-            firstName: this.message?.from.first_name || this.update.callback_query?.from.first_name,
-            isPremium: !!this.message?.from.is_premium || !!this.update.callback_query?.from.is_premium,
-            lang: (this.message?.from.language_code || this.update.callback_query?.from.language_code) as LanguageCode,
-            isBot: !!(this.message?.from.is_bot || this.update.callback_query?.from.is_bot)
-        }
     }
 }
