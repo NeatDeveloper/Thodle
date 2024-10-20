9d0a77a69bd024e1f8aa71d844e7f63c2b321942
ShahTheShah <NeatDeveloper@ya.ru>
2024-10-20 03:07:05 +0300

    update

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,13 @@
 import { Hono } from 'hono';
 
-const server = new Hono();
+const app = new Hono();
 
-server.get('/', async __context__ => {
+app.get('/', async __context__ => {
     return __context__.text('Hello, world!');
 });
 
-server.post('/', async __context__ => {
-    return __context__.json({ message: 'Hello' });
-});
 
 export default {
-    port: 3000,
-    fetch: server.fetch
+    port: Bun.env.API_PORT,
+    fetch: app.fetch
 }
