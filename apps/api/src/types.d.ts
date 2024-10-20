ff5da90f2c7d125f750a0b1c238903e5c7c5b6ff
ShahTheShah <NeatDeveloper@ya.ru>
2024-10-20 08:03:15 +0300

    update api

--- apps/api/src/types.d.ts
+++ apps/api/src/types.d.ts
@@ -1,6 +1,7 @@
 import '@repo/types/env';
 import type { HTTPException } from 'hono/http-exception';
-import type { StatusCode } from 'hono/utils/http-status';
+import type { ClientErrorStatusCode, ServerErrorStatusCode } from 'hono/utils/http-status';
+import type { StatusCodes } from 'http-status-codes';
 
 declare global {
     namespace APP {
@@ -10,7 +11,12 @@ declare global {
             };
         }
 
-        type Exception = (message?: string) => HTTPException;
+        type CreateException = (
+            status: {
+                [NAME in keyof typeof StatusCodes]: typeof StatusCodes[NAME] extends ClientErrorStatusCode | ServerErrorStatusCode ? NAME : never;
+            }[keyof typeof StatusCodes],
+            message?: string
+        ) => HTTPException;
     }
 }
 
--- apps/dapp/src/lib/index.ts
+++ apps/dapp/src/lib/index.ts
@@ -1,4 +1,1 @@
-import { init,  } from '@telegram-apps/sdk';
-
 // place files you want to import through the `$lib` alias in this folder.
-init();
