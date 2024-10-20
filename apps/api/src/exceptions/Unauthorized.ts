c753f068521bade91f0e91c72e40746b3c115499
ShahTheShah <NeatDeveloper@ya.ru>
2024-10-20 08:03:15 +0300

    update api

--- apps/api/src/exceptions/Unauthorized.ts
+++
@@ -1,9 +0,0 @@
-import { HTTPException } from 'hono/http-exception';
-import { ReasonPhrases, StatusCodes } from 'http-status-codes';
-
-export default (message => new HTTPException(StatusCodes.UNAUTHORIZED, {
-    res: new Response(message || ReasonPhrases.UNAUTHORIZED, {
-        status: StatusCodes.UNAUTHORIZED,
-        statusText: message || ReasonPhrases.UNAUTHORIZED,
-    })
-})) as APP.Exception;
