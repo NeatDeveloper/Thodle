a505919c26d86a95a8aa68d58287ee0761a0c078
ShahTheShah <NeatDeveloper@ya.ru>
2024-10-25 21:14:44 +0300

    update

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,5 +1,12 @@
 import { factory } from 'helpers';
+import users from './users';
 
-const routes = factory.createApp();
+const routes = factory.createApp().basePath('/');
+
+routes.get('/', async __context__ => {
+    return __context__.json({ message: 'hello' })
+});
+
+routes.route('/', users);
 
 export default routes;
