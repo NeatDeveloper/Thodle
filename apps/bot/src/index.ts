2818f64103ea01de4d2ad2ce3ad785d067478ad3
NeatDeveloper <ShahTheShah@ya.ru>
2024-12-10 10:55:30 +0300

    update

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -5,37 +5,37 @@ import { getMode, getTelegram } from '@repo/utils/env';
 const telegram = getTelegram();
 const mode = getMode();
 
-if(mode.MODE === 'PROD') {
+if (mode.MODE === 'PROD') {
     await bot.init();
 
-const handleUpdate = webhookCallback(bot, 'std/http', {
-    secretToken: telegram.TELEGRAM_SECRET_KEY
-});
+    const handleUpdate = webhookCallback(bot, 'std/http', {
+        secretToken: telegram.TELEGRAM_SECRET_KEY
+    });
 
-Bun.serve({
-    port: process.env.BOT_PORT,
-    fetch: (req, server) => {
-        const url = new URL(req.url);
+    Bun.serve({
+        port: process.env.BOT_PORT,
+        fetch: (req, server) => {
+            const url = new URL(req.url);
 
-        const XTelegramBotApiSecretToken = req.headers.get('X-Telegram-Bot-Api-Secret-Token');
+            const XTelegramBotApiSecretToken = req.headers.get('X-Telegram-Bot-Api-Secret-Token');
 
-        if (
-            req.method === 'POST' &&
-            url.pathname == `/${telegram.TELEGRAM_ENDPOINT}` &&
-            XTelegramBotApiSecretToken &&
-            XTelegramBotApiSecretToken === telegram.TELEGRAM_SECRET_KEY
-        ) try {
-            return handleUpdate(req);
-        } catch {
-            return new Response("Обработка запросов Telegram!");;
-        }
-        return new Response("Обработка запросов Telegram!");
-    },
+            if (
+                req.method === 'POST' &&
+                url.pathname == `/${telegram.TELEGRAM_ENDPOINT}` &&
+                XTelegramBotApiSecretToken &&
+                XTelegramBotApiSecretToken === telegram.TELEGRAM_SECRET_KEY
+            ) try {
+                return handleUpdate(req);
+            } catch {
+                return new Response("Обработка запросов Telegram!");;
+            }
+            return new Response("Обработка запросов Telegram!");
+        },
 
-});
+    });
 
 }
 
-if(mode.MODE === 'DEV') {
+if (mode.MODE === 'DEV') {
     await bot.start();
 }
