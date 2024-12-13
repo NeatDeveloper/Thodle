ba9928ca2ef0e637c931487cd606b7d0d6074306
NeatDeveloper <ShahTheShah@ya.ru>
2024-12-13 12:15:19 +0300

    add mode api connect

--- apps/bot/src/commands/schedule/index.ts
+++ apps/bot/src/commands/schedule/index.ts
@@ -1,39 +1,14 @@
 import { Composer } from 'grammy';
+import API from 'helpers/api';
 
 const commands = new Composer<APP.Context>();
 
+const api = API();
+
 commands.command('schedule', async (__context__) => {
-    await __context__.reply("Расписание на понедельник: \n 1. Алгебра и геометрия \n 2.Физика \n 3. ОРГ \n 4. Физ-ра", {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Пн",
-                        callback_data: "pn"
-                    },
-                    {
-                        text: "Вт",
-                        callback_data: "vt"
-                    },
-                    {
-                        text: "Ср",
-                        callback_data: "sr"
-                    },
-                    {
-                        text: "Чт",
-                        callback_data: "cht"
-                    },
-                    {
-                        text: "Пт",
-                        callback_data: "pt"
-                    },
+    const response = await api.get('schedule');
 
-
-                ]
-
-            ]
-        }
-    })
+    __context__.reply(await response.text());
 });
 
 export default commands;
